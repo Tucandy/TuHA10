f47110841d9f807bce1e5e77bb269b5d7f78067e
Anh Tú <53927427+Tucandy@users.noreply.github.com>
2021-12-28 12:53:26 +0700

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+@
+public/.htaccess,c\9\c9fe70f275af093c7fb3ed73b451fadc91069bac
---
+++ Core/Router.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Core;
+
+class Router
+{
+    protected $routes = [];
+
+    protected $params = [];
+
+    public function add($route, $params = [])
+    {
+        $route = preg_replace('/\//', '\\/', $route);
+
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        $route = '/^' . $route . '$/i';
+
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    public function dispatch($url)
+    {
+        $url = $this->removeQueryStringVariables($url);
+
+        if ($this->match($url)) {
+            $controller = $this->params['controller'];
+            $controller = $this->convertToStudlyCaps($controller);
+            $controller = $this->getNamespace() . $controller;
+
+            if (class_exists($controller)) {
+                $controller_object = new $controller($this->params);
+
+                $action = $this->params['action'];
+                $action = $this->convertToCamelCase($action);
+
+                if (preg_match('/action$/i', $action) == 0) {
+                    $controller_object->$action();
+
+                } else {
+                    throw new \Exception("Phương thức $action controller $controller gọi sai");
+                }
+            } else {
+                throw new \Exception("Controller class $controller không tìm thấy");
+            }
+        } else {
+            throw new \Exception('Lỗi', 404);
+        }
+    }
+
+
+    protected function getNamespace()
+    {
+        $namespace = 'App\Controllers\\';
+
+        if (array_key_exists('namespace', $this->params)) {
+            $namespace .= $this->params['namespace'] . '\\';
+        }
+
+        return $namespace;
+    }
+}
